67d50eec4db21cbfe23286de4b554cde7bf7dae8
Chambers <bobhus394@gmail.com>
2022-06-15 16:30:10 +0800

    刪除已無使用頁面

--- src/router/index.js
+++ src/router/index.js
@@ -10,16 +10,6 @@ const router = createRouter({
       component: HomeView
     },
     {
-      path: '/about',
-      name: 'about',
-      component: () => import('../views/AboutView.vue')
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: () => import('../views/AboutView.vue')
-    },
-    {
       path: '/:pathMatch(.*)*',
       redirect: {
         name: 'home'
